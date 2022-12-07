d4c39f4cc8f1c97941260e135170b3a6f7b38579
smyky <117633135+smyky@users.noreply.github.com>
2022-12-07 11:04:35 +0200

    Update routes.py (#20)

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.models import Menu
 
 @app.route('/')
 def home():
-	return jsonify({ "status": "ok: change2" })
+	return jsonify({ "status": "ok: change3" })
 
 @app.route('/menu')
 def menu():
